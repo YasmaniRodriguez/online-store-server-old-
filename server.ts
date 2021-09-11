b14f63a7eb6a3641ff5b23892cb12a38dd08194f
rodriguez yasmani <rodriguez.yasmani@hotmail.com>
2021-09-11 02:09:41 -0300

    ajustes varios

--- server.ts
+++ server.ts
@@ -4,61 +4,18 @@ import { createServer } from "http";
 
 const express = require("express");
 const moment = require("moment");
-const fs = require("fs");
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 const port = 8080;
 
+const server_time = moment().format("DD/MM/YYYY HH:MM:SS");
 const products: {
 	name: string;
 	description: string;
 	price: number;
 	image: string;
 }[] = [];
-const channel = "./channel.json";
-const messages = getMessage();
-const now = moment().format("DD/MM/YYYY HH:MM:SS");
-
-const schema = require("./db/queriesStoreDB");
-
-schema.dbCreateSchema();
-
-function addMessage(message: {
-	nickname: string;
-	datetime: string;
-	message: string;
-}) {
-	try {
-		const data = fs.readFileSync(channel);
-		const json = JSON.parse(data.toString("utf-8"));
-		json.push({ ...message });
-		fs.writeFileSync(channel, JSON.stringify(json, null, "\t"));
-	} catch {
-		try {
-			fs.writeFileSync(channel, JSON.stringify([{ ...message }]));
-		} catch (e) {
-			let err = <Error>e;
-			console.log(err.stack);
-		}
-	}
-}
-
-function getMessage() {
-	try {
-		const data = fs.readFileSync(channel);
-		const json = JSON.parse(data.toString("utf-8"));
-		return json;
-	} catch {
-		try {
-			fs.writeFileSync(channel, JSON.stringify([]));
-		} catch (e) {
-			let err = <Error>e;
-			console.log(err.stack);
-		}
-	}
-}
-
 /////////////////////////////////////////////////////////////////
 app.use(express.static(__dirname + "/public"));
 
@@ -70,7 +27,7 @@ io.on("connection", (socket: Socket) => {
 	console.log(`connection_identifier: ${socket.id}`);
 
 	socket.emit("products", products);
-	socket.emit("messages", messages);
+	getMessages();
 
 	socket.on("new-product", (product: any) => {
 		products.push(product);
@@ -78,8 +35,8 @@ io.on("connection", (socket: Socket) => {
 	});
 
 	socket.on("new-message", (message: any) => {
-		addMessage({ ...message, datetime: now });
-		io.emit("messages", messages);
+		addMessage({ ...message, datetime: server_time });
+		getMessages();
 	});
 });
 
@@ -90,3 +47,93 @@ server
 	.on("error", (err) => {
 		console.log(`something is preventing us grow , more detail in: ${err}`);
 	});
+/////////////////////////////////////////////////////////////////
+const { mysql, sqlite } = require("./odbc");
+const store = require("knex")(mysql);
+const chat = require("knex")(sqlite);
+
+(function () {
+	store.schema.hasTable("products").then((exists: any) => {
+		if (!exists) {
+			return store.schema
+				.createTable("products", (table: any) => {
+					table.increments("id").primary();
+					table.string("name", 20);
+					table.string("description", 50);
+					table.decimal("price");
+					table.string("image");
+				})
+				.then(() =>
+					console.log("congrats, store db schema is created successfully!")
+				)
+				.catch((err: any) => {
+					console.log(err);
+				})
+				.finally(() => {
+					store.destroy();
+				});
+		} else {
+			console.log("fantastic, store db schema already exists!");
+		}
+	});
+})();
+
+(function () {
+	chat.schema.hasTable("messages").then((exists: any) => {
+		if (!exists) {
+			return chat.schema
+				.createTable("messages", (table: any) => {
+					table.increments("id").primary();
+					table.string("alias", 20);
+					table.string("datetime", 50);
+					table.string("message", 250);
+				})
+				.then(() =>
+					console.log("congrats, chat db schema is created successfully!")
+				)
+				.catch((err: any) => {
+					console.log(err);
+				})
+				.finally(() => {
+					chat.destroy();
+				});
+		} else {
+			console.log("fantastic, chat db schema already exists!");
+		}
+	});
+})();
+
+const addMessage = (message: {
+	alias: string;
+	datetime: string;
+	message: string;
+}) => {
+	chat
+		.insert([
+			{
+				alias: message.alias,
+				datetime: message.datetime,
+				message: message.message,
+			},
+		])
+		.into("messages")
+		.then(() => {
+			console.log("message delivered");
+		})
+		.catch((err: any) => {
+			console.log(err);
+		});
+};
+
+const getMessages = () => {
+	chat
+		.from("messages")
+		.select("alias", "datetime", "message")
+		.orderBy("id")
+		.then((rows: { alias: string; datetime: string; message: string }) => {
+			io.emit("messages", rows);
+		})
+		.catch((err: any) => {
+			console.log(err);
+		});
+};
