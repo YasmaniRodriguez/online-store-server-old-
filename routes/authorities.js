aa5088e3bf13f2cbd2b69e4fed0c4e97c2d18a6b
rodriguez yasmani <rodriguez.yasmani@hotmail.com>
2021-10-12 18:55:21 -0300

    esquema de permisos

--- routes/authorities.js
+++ routes/authorities.js
@@ -1,12 +1,78 @@
 const checkAuthority = (req, res, next) => {
-	//refactorizar para capturar la ruta y el metodo y a partir de ahí definir los privilegios
+	const method = req.method;
+	const url = req.route.path;
 	const { role } = req.user;
-	if (role !== "owner") {
-		return res
-			.status(403)
-			.json({ error: "user don't have required permissions" });
-	} else {
-		next();
+
+	switch (true) {
+		//////////////////////////
+		case method === "GET" && url === "/products":
+			next();
+			break;
+		case method === "POST" && url === "/products":
+			return role !== "owner"
+				? res
+						.status(403)
+						.json({ error: "user don't have required permissions" })
+				: next();
+			break;
+		case method === "PUT" ?? (url === "/products" || url === "/products:id"):
+			return role !== "owner"
+				? res
+						.status(403)
+						.json({ error: "user don't have required permissions" })
+				: next();
+			break;
+		case method === "DELETE" ?? (url === "/products" || url === "/products:id"):
+			return role !== "owner"
+				? res
+						.status(403)
+						.json({ error: "user don't have required permissions" })
+				: next();
+			break;
+		//////////////////////////
+		case method === "GET" && url === "/messages":
+			next();
+			break;
+		case method === "POST" && url === "/messages":
+			next();
+			break;
+		case method === "PUT" ?? (url === "/messages" || url === "/messages/:id"):
+			return role !== "owner"
+				? res
+						.status(403)
+						.json({ error: "user don't have required permissions" })
+				: next();
+			break;
+		case method === "DELETE" ??
+			(url === "/messages" || url === "/messages/:id"):
+			return role !== "owner"
+				? res
+						.status(403)
+						.json({ error: "user don't have required permissions" })
+				: next();
+			break;
+		//////////////////////////
+		case method === "GET" && url === "/orders":
+			next();
+			break;
+		case method === "POST" && url === "/orders":
+			next();
+			break;
+		case method === "PUT" && url === "/orders":
+			next();
+			break;
+		case method === "DELETE" ?? (url === "/orders" || url === "/orders/:id"):
+			return role !== "owner"
+				? res
+						.status(403)
+						.json({ error: "user don't have required permissions" })
+				: next();
+			break;
+		//////////////////////////
+		default:
+			res.status(403).json({
+				error: `for convination [method: ${method}, url: ${url}, role: ${role}] there are not defined permissions`,
+			});
 	}
 };
 
